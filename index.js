029d4a321e892d2a184342b7b87c3b0d92f5c1e9
gasz <1pawsao1@gmail.com>
2022-03-18 03:08:15 +0100

    the first complete version (working in the browser)

--- index.js
+++ index.js
@@ -4,30 +4,42 @@ const ulEl = document.querySelector('#ul-el')
 const deleteBtn = document.querySelector('#delete-btn')
 const tabBtn = document.querySelector('#tab-btn')
 
+// let myLinks = ["https://www.youtube.com/", 'https://flaviocopes.com/', 'https://scrimba.com/']
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLinks'))
+// localStorage.setItem('myLeads', JSON.stringify(myLinks))
+
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 console.log(leadsFromLocalStorage)
 
+// addToLocalStorage("https://stackoverflow.com/", "myLeads")
 let myLinks = []
 if(leadsFromLocalStorage){
     myLinks = leadsFromLocalStorage
     startLinks(myLinks)
 
 }else(
-    localStorage.setItem('myLinks', JSON.stringify([]))
+    localStorage.setItem('myLeads', JSON.stringify([]))
 )
 console.log(myLinks)
 
 tabBtn.addEventListener('click', ()=>{
-    const windowUrl = window.location.href
+    // const windowUrl = window.location.href//dobrze ale my bierzemy url z innego okna deFacto
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        const windowUrl = tabs[0].url
+        if(myLinks.includes(windowUrl) === false){
+            addToLocalStorage(windowUrl, "myLeads")
+            createElement(windowUrl)
+            myLinks.push(windowUrl)
+        }
+    })
     
-    
-    console.log(windowUrl)
-    if(myLinks.includes(windowUrl) === false){
-        addToLocalStorage(windowUrl, "myLinks")
-        createElement(windowUrl)
-        myLinks.push(windowUrl)
-    }
+    // console.log(windowUrl)
+    // if(myLinks.includes(windowUrl) === false){
+    //     addToLocalStorage(windowUrl, "myLeads")
+    //     createElement(windowUrl)
+    //     myLinks.push(windowUrl)
+    // }
     
 })
 
@@ -42,7 +54,7 @@ inputBtn.addEventListener("click", (e)=>{
     if(inputValue && myLinks.includes(inputValue) === false){
         console.log('tutaj w listenerze inlutBtn')
         myLinks.push(inputValue)
-        addToLocalStorage(inputValue, "myLinks")
+        addToLocalStorage(inputValue, "myLeads")
         createElement(inputValue)
     }
     inputEl.value = ''
@@ -50,6 +62,8 @@ inputBtn.addEventListener("click", (e)=>{
 })
 function startLinks(array){
     array.forEach((element) => {
+    // JSON.parse(localStorage.getItem('myLeads')).forEach((element) => {
+    // myLinks.forEach((element) => {
         createElement(element)
     })
 }
@@ -58,6 +72,7 @@ function createElement(el){
     let aEl = document.createElement('a')
     aEl.setAttribute('href', el)
     aEl.setAttribute('target', '_blank')
+    // console.log(aEl)
     let liEl = document.createElement('li')
     liEl.textContent = el
     aEl.appendChild(liEl)
@@ -68,12 +83,15 @@ function createElement(el){
 function addToLocalStorage(sth, key){
     if(JSON.parse(localStorage.getItem(key))){
     }else(
-        localStorage.setItem('myLinks', JSON.stringify([]))
+        localStorage.setItem('myLeads', JSON.stringify([]))
     )
     let array = Object.values(JSON.parse(localStorage.getItem(key)))
     if(array.includes(sth) === false){
         array.push(sth)
     }
-
+    // console.log(array)
     localStorage.setItem(key, JSON.stringify(array))
+    // console.log(JSON.parse(localStorage.getItem(key)))
+
+
 }
